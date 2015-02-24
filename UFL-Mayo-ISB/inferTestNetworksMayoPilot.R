2bbc7b7065c5b2cddd8876d9dc5eb8aa6ea24cb0
Ben Logsdon <aeberhart@gmail.com>
2015-02-24 13:30:11 -0800

    updated network reconstruction and enrichment testing

--- UFL-Mayo-ISB/inferTestNetworksMayoPilot.R
+++ UFL-Mayo-ISB/inferTestNetworksMayoPilot.R
@@ -36,20 +36,40 @@ for (i in 740:1000){
   rfNet[i,-i] <- res[,5];
   cat('finished ',i,'of 1000\n')
 }
-sparrowNetAvg <- sparrowNet/2+t(sparrowNet)/2
+sparrowNetAvg <- sparrowNet/2+t(sparrowNet)/2 > .95
 lassoNetAvg <- lassoNet/2+t(lassoNet)/2
 ssNetAvg <- ssNet/2+t(ssNet)/2
 ridgeNetAvg <- ridgeNet/2+t(ridgeNet)/2
 rfNetAvg <- rfNet/2+t(rfNet)/2
 
-sparrowHub <- rowSums(sparrowNetAvg)
-lassoHub <- rowSums(lassoNetAvg)
-ssHub <- rowSums(ssNetAvg)
-ridgeHub <- rowSums(ridgeNetAvg)
-rfHub <- rowSums(rfNetAvg)
+sparrowHub <- signif(scale(rowSums(sparrowNetAvg)),2)
+lassoHub <- signif(scale(rowSums(lassoNetAvg)),2)
+ssHub <- signif(scale(rowSums(ssNetAvg)),2)
+ridgeHub <- signif(scale(rowSums(ridgeNetAvg)),2)
+rfHub <- signif(scale(rowSums(rfNetAvg)),2)
+aggHub <- signif(sparrowHub+lassoHub+ssHub+ridgeHub+rfHub,2)
 
 geneId <- ensembl2symbol(colnames(tcxAlzheimer1000))
 names(geneId) <- colnames(tcxAlzheimer1000)
-dfHub <- data.frame(geneId,sparrowHub,lassoHub,ssHub,ridgeHub,rfHub,row.names=colnames(tcxAlzheimer1000))
+dfHub <- data.frame(geneId,sparrowHub,lassoHub,ssHub,ridgeHub,rfHub,aggHub,row.names=colnames(tcxAlzheimer1000))
 
+
+
+alzGenes <- scan('geneLists/geneCardsAlzheimer.txt',what='character')
+alzGenetics <- unique(scan('geneLists//alzheimerGeneticsGenes.txt',what='character'))
+alzheimerGene<-(dfHub$geneId%in%alzGenes)
+alzheimerGWAS <- (dfHub$geneId%in%alzGenetics)
+dfHub <- data.frame(dfHub,alzheimerGene)
+
+
+enrichment(alzGenes,dfHub$geneId[order(dfHub$aggHub,decreasing=T)[1:100]],dfHub$geneId)
+
+
+tcresult<-as.tableColumns(dfHub)
+cols<-tcresult$tableColumns
+fileHandleId<-tcresult$fileHandleId
+projectId<-"syn3243392"
+schema<-TableSchema(name="TCX Alzheimer Preliminary Integrated Analysis", parent=projectId, columns=cols)
+table<-Table(schema, fileHandleId)
+table<-synStore(table, retrieveData=TRUE)
 
